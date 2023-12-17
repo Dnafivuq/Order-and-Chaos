64c328f1f0ceae04c54420692a82c5b7bc83e53c
dnvqs <dnvqs@c.cc>
2023-12-17 15:57:04 +0100

    Fixed 'check_if_clicked' function

--- GUI.py
+++ GUI.py
@@ -20,16 +20,16 @@ class Button:
         if colors[1]:
             self._border_color = colors[1]
 
-    def check_if_clicked(self, mouse_position) -> bool:
+    def check_if_clicked(self, mouse_position: (int, int)) -> bool:
         if mouse_position[0] >= self._position[0] and mouse_position[0] <= self._position[0]+self._size[0]:
-            if mouse_position[1] >= self._position[1] and mouse_position[1] <= self._position[1]+self._size[0]:
+            if mouse_position[1] >= self._position[1] and mouse_position[1] <= self._position[1]+self._size[1]:
                 return True
         return False
 
     def on_click(self) -> None:
-        pass
+        self.update_colors(("", "yellow"))
 
-    def render(self, screen):
+    def render(self, screen: pygame.surface) -> None:
         button_rect = pygame.Rect(self._position[0], self._position[1], self._size[0], self._size[1])
         pygame.draw.rect(screen, self._fill_color, button_rect, 0, 10)
         pygame.draw.rect(screen, self._border_color,  button_rect, 3, 10)
@@ -40,6 +40,6 @@ class ChangeSymbolButton(Button):
         super().__init__()
         self._symbol = symbol
 
-    def on_click(self):
+    def on_click(self) -> None:
         self.update_colors(("", "yellow"))
         return self._symbol
